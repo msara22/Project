7d770d35765d2579b1ca1c4e8691fb2e662d8b18
msara22 <72658217+msara22@users.noreply.github.com>
2020-12-27 22:56:06 +0600

    Update EncryptionController.java

--- EncryptionController.java
+++ EncryptionController.java
@@ -25,72 +25,138 @@ public class EncryptionController {
 
     @FXML
     private TextField result;
-
-    @FXML
-    void encode(ActionEvent event) {
-        try{
-            int keyInStr = Integer.parseInt(keyInput.getText());
-
-            String textInt = textInput.getText();
-
-            StringBuilder stb = new StringBuilder();
-            char[] charList = textInt.toCharArray();
-            int k = 0;
-            for (int i = 0; i < charList.length; i++) {
-                if ((charList[i] >= 'a') && (charList[i] <= 'z')) {
-                    stb.append((char) updatePosition('a', charList[i], keyInStr));
-                } else if ((charList[i] >= 'A') && (charList[i] <= 'Z')) {
-                    stb.append((char) updatePosition('A', charList[i], keyInStr));
-                } else {
-                    stb.append(charList[i]);
-                }
+    int modularMultiplicativeInverse(int keyInStr) {
+        keyInStr = keyInStr % 26;
+        for (int i = 1; i <= 26; i++) {
+            if (keyInStr * i % 26 == 1) {
+                return i;
             }
-            result.setText(stb.toString());
         }
-        catch (NumberFormatException e){
-            keyInput.setText("Input number");
-        }
-
-
+        return 1;
     }
 
-    int updatePosition(char sChar, char cChar, int keyInStr) {
-        int position = cChar - sChar;
-        int updatedPosition = (position + keyInStr) % 26;
-        return sChar + updatedPosition;
-    }
-    int updatePosition1(char sChar, char cChar, int keyInStr) {
-        int position = cChar - sChar;
-        int updatedPosition = Math.abs((position - keyInStr)) % 26;
-        return sChar + updatedPosition;
+    int gcd(int number1, int number2) {
+        if (number2 == 0)
+            return number1;
+        else
+            return gcd(number2, number1 % number2);
     }
 
     @FXML
     void decode(ActionEvent event) {
         try{
             int keyInStr = Integer.parseInt(keyInput.getText());
+            int keyInStr1 = Integer.parseInt(keyInput.getText());
 
             String textInt = textInput.getText();
-            StringBuilder stb = new StringBuilder();
-            char[] charList = textInt.toCharArray();
-            int k = 0;
-            for (int i = 0; i < charList.length; i++) {
-                if ((charList[i] >= 'a') && (charList[i] <= 'z')) {
-                    stb.append((char) updatePosition1('a', charList[i], keyInStr));
-                } else if ((charList[i] >= 'A') && (charList[i] <= 'Z')) {
-                    stb.append((char) updatePosition1('A', charList[i], keyInStr));
+
+            if (gcd(keyInStr1, 26) != 1) {
+                throw new IllegalArgumentException("Error: keyA and alphabet size must be coprime.");
+            }
+            textInt = textInt.toLowerCase();
+            textInt = textInt.replaceAll(" ", "");
+            int MMI = modularMultiplicativeInverse(keyInStr);
+            int fixedLength = 0;
+            StringBuilder plaintext = new StringBuilder();
+            for (int i = 0; i < textInt.length(); i++) {
+                if (textInt.charAt(i) == ',' || textInt.charAt(i) == '.') {
+                    continue;
+                } else if (Character.isDigit(textInt.charAt(i))) {
+                    plaintext.append(textInt.charAt(i));
                 } else {
-                    stb.append(charList[i]);
+                    int y = textInt.charAt(i);
+                    int result = (y - 97) - keyInStr1;
+                    if (result < 0) {
+                        while (result < 0) {
+                            result = result + 26;
+                        }
+                    }
+                    plaintext.append((char) (MMI * (result) % 26 + 97));
                 }
             }
+            result.setText(plaintext.toString());
 
-            result.setText(stb.toString());
+        }
+        catch(IllegalArgumentException e){
+            keyInput.setText("Enter coprime number");
+        }
+        try{
+            int keyInStr = Integer.parseInt(keyInput.getText());
+            int keyInStr1 = Integer.parseInt(keyInput.getText());
+
+            String textInt = textInput.getText();
+
+            if (gcd(keyInStr1, 26) != 1) {
+                throw new IllegalArgumentException("Error: keyA and alphabet size must be coprime.");
+            }
+            textInt = textInt.toLowerCase();
+            textInt = textInt.replaceAll(" ", "");
+            int MMI = modularMultiplicativeInverse(keyInStr);
+            int fixedLength = 0;
+            StringBuilder plaintext = new StringBuilder();
+            for (int i = 0; i < textInt.length(); i++) {
+                if (textInt.charAt(i) == ',' || textInt.charAt(i) == '.') {
+                    continue;
+                } else if (Character.isDigit(textInt.charAt(i))) {
+                    plaintext.append(textInt.charAt(i));
+                } else {
+                    int y = textInt.charAt(i);
+                    int result = (y - 97) - keyInStr1;
+                    if (result < 0) {
+                        while (result < 0) {
+                            result = result + 26;
+                        }
+                    }
+                    plaintext.append((char) (MMI * (result) % 26 + 97));
+                }
+            }
+            result.setText(plaintext.toString());
 
         }
         catch(NumberFormatException e){
-            textInput.setText("Enter number");
+            keyInput.setText("Enter coprime number");
         }
 
     }
 
+    @FXML
+    void encode(ActionEvent event) {
+        try{
+            int keyInStr = Integer.parseInt(keyInput.getText());
+            int keyInStr1 = Integer.parseInt(keyInput.getText());
+
+            String textInt = textInput.getText();
+
+            if (gcd(keyInStr, 26) != 1) {
+                throw new IllegalArgumentException("Error: key A and alphabet size must be coprime.");
+            }
+            textInt = textInt.toLowerCase();
+            textInt = textInt.replaceAll(" ", "");
+            StringBuilder ciphertext = new StringBuilder();
+            int fixedLength = 0;
+            for (int i = 0; i < textInt.length(); i++) {
+                if (textInt.charAt(i) == ',' || textInt.charAt(i) == '.') {
+                    continue;
+                } else if (fixedLength == 5) {
+                    ciphertext.append(" ");
+                    fixedLength = 0;
+                    i--;
+                } else if (Character.isDigit(textInt.charAt(i))) {
+                    ciphertext.append(textInt.charAt(i));
+                    fixedLength++;
+                } else {
+                    ciphertext.append((char) ((((textInt.charAt(i) - 97) * keyInStr + keyInStr1) % 26) + 97));
+                    fixedLength++;
+                }
+            }
+            result.setText(ciphertext.toString());
+        }
+        catch(IllegalArgumentException e){
+            keyInput.setText("Enter coprime number");
+        }
+
+
+    }
+
 }
+
